c796b3208d9d8dcfe98ac6f98cfc01e0ab724b4f
Gracerosen <64803076+Gracerosen@users.noreply.github.com>
2021-10-22 21:05:22 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React, {usestate} from “react”; 7.4K (gzipped: 3K)
+
+import “/.App.css”;
+
+return (
+
+<div className="App">
+  <div className=(isCircle ? "circle": "square"}></div>
+    <button onClick={() => setIsCircle(!isCircle)}> Change Shape</button>
+</div>
+);
