0f8e4d1f4942a9ff311b0b501a7b2918e110c904
Gracerosen <64803076+Gracerosen@users.noreply.github.com>
2021-10-23 06:43:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ return (
     <button onClick={() => setIsCircle(!isCircle)}> Change Shape</button>
 </div>
 );
+
+const nameInput = useRef(null);
+const clearInput = () =>{
+  nameInput.current.value="";
+  console.log (nameInput.current)
+};
+return (
+  <div className= "App">
+  <inpput type="text" placeholder="Write Your Name..." ref={nameInput}/>
+  <button onClick={clearInput}>Sumbit</button>
+  </div>
+);
+}
